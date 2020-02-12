40e763fd3f012005a976073e3753a07a431f536e
LukaMesec <luka@mesec.org>
2020-02-12 11:23:17 +0100

    1

--- src/parseSellerDetail.js
+++ src/parseSellerDetail.js
@@ -184,7 +184,7 @@ async function parseSellerDetail($, request) {
     const qu1 = await requestQueue.addRequest({
         url: String(request.userData.sellerUrl)
     });
-    extractSellers(qu1);
+    extractSellers($, qu1);
     item.productUrl = request.url;
     // const currency = await getCurrency(request);
     // console.log(item);
